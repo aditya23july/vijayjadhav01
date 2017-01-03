6b6869e90db71b98bae6af4fa567cbd5cf377031
aditya23july <adityalohiya23@gmail.com>
2017-01-03 23:36:40 +0530

    FB and GOOGLE integration

--- application/views/web/home/login.php
+++ application/views/web/home/login.php
@@ -1,4 +1,5 @@
-     <script src="<?php echo base_url() ?>asset/admin/js/jquery-2.1.1.min.js"></script>
+     <meta name="google-signin-client_id" content="883416317442-qkvcmogbvhdjbl418abkkasoo8deee8q.apps.googleusercontent.com">
+	 <script src="<?php echo base_url() ?>asset/admin/js/jquery-2.1.1.min.js"></script>
   <script src="<?php echo base_url() ?>asset/admin/js/jquery.validate.js"></script>
  <script>
 $(document).ready(function(){
@@ -29,8 +30,30 @@ $(document).ready(function(){
       });
 
 });
-</script>
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+ // console.log('Name: ' + profile.getName());
+  //console.log('Image URL: ' + profile.getImageUrl());
+  //console.log('Email: ' + profile.getEmail());
+  //var id_token = googleUser.getAuthResponse().id_token;
+     //   console.log("ID Token: " + id_token);
+		sendData(profile,'GOOGLE');
+}
 
+</script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<div id="fb-root"></div>
+<script>
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=614547435399258";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="content" class="site-content" tabindex="-1">
     <div class="container">
 
@@ -98,11 +121,108 @@ $(document).ready(function(){
 
                                             <div class="col-sm-3">
                                                 <label class="texttocenter">Login With</label>
-                                                <div class="logsoc">
+                                                <div class="logsoc" style="top:0px">
 
-                                                    <img src="images/facebook.png"><br>
-                                                    <p>OR</p>
-                                                    <img src="images/google.png">
+                                                  <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+<a class="waves-effect waves-light btn btn-block facebook"><i class="fa fa-facebook-official left" aria-hidden="true" ></i>Sign in with Facebook</a>
+	</fb:login-button><br>
+                                                    <script>
+	  
+// This is called with the results from from FB.getLoginStatus().
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+	} else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+		getaccesstoken(response);
+      statusChangeCallback(response);
+    });
+  }
+function getaccesstoken(response){;
+var accesstoken=response.authResponse.accessToken;
+}
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '614547435399258',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.7' // use graph api version 2.5
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+ 
+    
+    FB.api('/me', { locale: 'en_US', fields: 'name, email,id' }, function(response) {
+		
+		sendData(response,'FACEBOOK');
+	});
+	
+  }
+  function sendData(response,type){
+
+		
+		$.post("<?php echo base_url() ?>index.php/web/socialLogin",{response:response,type:type},
+				function(data){
+					if(data=='SUCCESS'){
+					window.location.href = "<?php echo base_url()?>index.php/customer/index";
+					}else{
+					alert('error');
+					}
+				});
+  }
+</script>
+													<p>OR</p>
+                                                    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+
                                                 </div>
                                             </div>
 
